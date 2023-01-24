4a9b846a30a753767e7fefafc41b84d1d4bab2e3
Pierre BIZIEN <pbizien@e2r10p16.clusters.42paris.fr>
2023-01-24 13:55:06 +0100

    13h55

--- ft_lst.c
+++ ft_lst.c
@@ -1,35 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_move.c                                          :+:      :+:    :+:   */
+/*   ft_lst.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbizien <pbizien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/24 11:24:14 by pbizien           #+#    #+#             */
-/*   Updated: 2023/01/24 13:33:56 by pbizien          ###   ########.fr       */
+/*   Created: 2023/01/24 11:45:48 by pbizien           #+#    #+#             */
+/*   Updated: 2023/01/24 13:52:18 by pbizien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-void    ft_sa(t_elem *list_a)
+t_elem	*ft_lst_new(int content, int index)
 {
-    t_elem  *begin;
-    int     tmp;
+	t_elem	*ptr;
 
-    begin = list_a;
-    tmp = begin->val;
-    begin->val = begin->next->val;
-    begin->next->val = tmp;
+	ptr = ft_calloc(1, sizeof(t_elem));
+	if (!ptr)
+		return (NULL);
+	ptr->val = content;
+    ptr->ind = index;
+	return (ptr);
 }
 
-void    ft_sb(t_elem *list_b)
+t_elem  *ft_lst_new_empty(void)
 {
-    t_elem  *begin;
-    int     tmp;
+    t_elem	*ptr;
 
-    begin = list_b;
-    tmp = begin->val;
-    begin->val = begin->next->val;
-    begin->next->val = tmp;
+	ptr = ft_calloc(1, sizeof(t_elem));
+	if (!ptr)
+		return (NULL);
+    ptr->ind = 0;
+	return (ptr);
 }
--- main.c
+++ main.c
@@ -6,82 +6,65 @@
 /*   By: pbizien <pbizien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 10:01:00 by pbizien           #+#    #+#             */
-/*   Updated: 2023/01/24 11:26:58 by pbizien          ###   ########.fr       */
+/*   Updated: 2023/01/24 13:54:24 by pbizien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 #include <stdio.h>
 
-void	ft_free_double_c(char **tmp)
+int	ft_len_tmp(char **tmp)
 {
 	int	i;
-
+	
 	i = 0;
 	while (tmp[i])
+		i++;
+	return (i);
+}
+
+t_elem	*ft_generate_a_l(char *av, t_data *data)
+{
+	char	**tmp;
+	t_elem	*begin;
+	t_elem	*tmp_l;
+	int 	i;
+	
+	i = 1;
+	tmp = ft_split(av, ' ');
+	begin = ft_lst_new(ft_atoi(tmp[0]), 0);
+	tmp_l = begin;
+	while (tmp[i])
 	{
-		free(tmp[i]);
+		begin->next = ft_lst_new(ft_atoi(tmp[i]), i);
+		begin = begin->next;
 		i++;
 	}
-	free(tmp);
+	begin->next = NULL;
+	data->size = i;
+	return (tmp_l);
 }
 
-int *ft_generate_a(char *av, t_data *data)
+t_elem	*ft_generate_b_l(void)
 {
-	char 	**tmp;
-	int		i;
-	int		count;
-	int		*output;
-	int		j;
-	
-	count = 0;
-	tmp = ft_split(av, ' ');
-	while (tmp[count])
-		count++;
-	data->size = count;
-	output = ft_calloc(sizeof(int), count * 2);
-	if (!output)
-		return (NULL);
-	i = count - 1;
-	j = 0;
-	while (i >= 0)
-	{
-		output[j] = ft_atoi(tmp[i]);
-		i--;
-		j++;
-	}
-	ft_free_double_c(tmp);
-	return (output);
+	t_elem *begin;
+
+	begin = ft_lst_new_empty();
+	begin->next = NULL;
+	return (begin);
 }
-
-void	ft_print_tabs(int *tab_a, int *tab_b, t_data *data)
-{
-	int	i;
-
-	i = data->size - 1;
-	ft_printf("----------\n");
-	while (i >= 0)
-	{
-		ft_printf("A %d | B %d\n", tab_a[i], tab_b[i]);
-		ft_printf("----------\n");
-		i--;
-	}
-}
-
-
 
 int main(int ac, char **av)
 {
-    int		*tab_a;
-	int		*tab_b;
+    // int		*tab_a;
+	// int		*tab_b;
 	t_data	data;
+	t_elem	*begin_a;
+	t_elem	*begin_b;
 
-	tab_a = ft_generate_a(av[1], &data);
-	if (!tab_a)
-		return (-1);
-	tab_b = ft_calloc(sizeof(int), data.size);
-	if (!tab_b)
-		return (free(tab_a), -1);
+	begin_a = ft_generate_a_l(av[1], &data);
+	begin_b = NULL;
     (void)ac;
-	ft_print_tabs(tab_a, tab_b, &data);
+	ft_print_a_b(begin_a, begin_b);
+	// begin_a = begin_a->next;
 }
